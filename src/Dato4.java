b9166273bd5175fa3292b665544bca03b4cd082c
EdwinPoz <99162865+Edwincrak@users.noreply.github.com>
2022-09-03 22:57:15 -0600

    tarea2
    Subiendo carpetas que no me las subió

---
+++ src/Dato4.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Dato4 {
+    
+    public String contrasena;
+    public String nombre;
+  
+    
+ public Dato4(String nombre, String contrasena){
+ this. nombre=nombre;
+ this. contrasena=contrasena;
+
+ }
+
+
+ 
+}
